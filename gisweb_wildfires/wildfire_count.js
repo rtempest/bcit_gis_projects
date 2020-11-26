0164b0bce8bf6dba866bb36e252eb9a4f3e76f68
rtempest <56704892+rtempest@users.noreply.github.com>
2020-11-25 17:53:59 -0800

    Add files via upload
    module 9 - creating a web map with leaflet, and a chart with chart.js

---
+++ gisweb_wildfires/wildfire_count.js
@@ -0,0 +1,44 @@
+var wildfire_count = {
+    "jsonarray": [
+        {
+            "month": "April",
+            "count": 92,
+            "area": 582.765
+        },
+        {
+            "month": "May",
+            "count": 62,
+            "area": 88.543
+        },
+        {
+            "month": "June",
+            "count": 31,
+            "area": 29.365
+        },
+        {
+            "month": "July",
+            "count": 114,
+            "area": 1254.321
+        },
+        {
+            "month": "August",
+            "count": 277,
+            "area": 12375.176
+        },
+        {
+            "month": "September",
+            "count": 57,
+            "area": 59.65
+        },
+        {
+            "month": "October",
+            "count": 13,
+            "area": 72.349
+        },
+        {
+            "month": "November",
+            "count": 2,
+            "area": 0.018
+        }
+    ]
+}
